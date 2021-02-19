bf4d9b190128f5c5d8079cb25ccabc1b8b32aed3
Maschell <Maschell@gmx.de>
2021-02-19 17:09:02 +0100

    Use WUMS_MODULE_SKIP_WUT_INIT

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,7 @@
 
 WUMS_MODULE_EXPORT_NAME("homebrew_patchmemoryrelocations");
 WUMS_MODULE_SKIP_ENTRYPOINT();
+WUMS_MODULE_SKIP_WUT_INIT();
 WUMS_MODULE_INIT_BEFORE_RELOCATION_DONE_HOOK();
 
 bool elfLinkOne(char type, size_t offset, int32_t addend, uint32_t destination, uint32_t symbol_addr, relocation_trampolin_entry_t *trampolin_data, uint32_t trampolin_data_length, RelocationType reloc_type);
